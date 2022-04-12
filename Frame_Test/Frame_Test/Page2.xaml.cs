32e6e76238f540cbd0aa89699579b70c4bf1ca66
MATTHEWALLEN122 <matthewballen123@gmail.com>
2022-04-12 16:14:16 -0500

    Most up-to-date 4/12/2022

---
+++ Frame_Test/Frame_Test/Page2.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Frame_Test
+{
+    /// <summary>
+    /// Interaction logic for Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        int total_wins;
+        public Page2(bool won, string total_time, int total_tries, string winning_word, int wins)
+        {
+            InitializeComponent();
+            if (won)
+            {
+                win_or_lose_box.Text = "YOU WON :D";
+                total_wins += wins;
+            }
+            else
+            {
+                win_or_lose_box.Text = "YOU LOSE :P";
+            }
+            game_info.Text = $"winning word: {winning_word}\n" +
+                    $"Time remaining:\t{total_time}\n" +
+                    $"Guessed used:\t{total_tries}\n" +
+                    $"Total wins:\t{total_wins}\n";
+        }
+
+        private void new_game_button_Click(object sender, RoutedEventArgs e)
+        {
+            var window = (MainWindow)Application.Current.MainWindow;
+            window.Frame.Content = new Page1(total_wins);
+        }
+    }
+}
